e37e516c920c68c5a779dadc72155dca87ebfbfe
kwon <hdragon1501@gmail.com>
2019-04-30 23:06:22 +0700

    FrontEnd with Home and Login Page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,80 @@
+/*eslint-disable*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import { List, ListItem, withStyles } from "@material-ui/core";
+
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+
+import footerStyle from "assets/jss/material-kit-react/components/footerStyle.jsx";
+
+function Footer({ ...props }) {
+  const { classes, whiteFont } = props;
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  const aClasses = classNames({
+    [classes.a]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="https://www.nguyenquan.net"
+                className={classes.block}
+                target="_blank"
+              >
+                About me
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="http://blog.nguyenquan.net/"
+                className={classes.block}
+                target="_blank"
+              >
+                Blog
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="https://github.com/anguyenguy"
+                className={classes.block}
+                target="_blank"
+              >
+                Github
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <div className={classes.right}>
+          &copy; {1900 + new Date().getYear()} , made with{" "}
+          <Favorite className={classes.icon} /> by{" "}
+          <a
+            href="https://www.nguyenquan.net"
+            className={aClasses}
+            target="_blank"
+          >
+            Nguyen Quan
+          </a>{" "}
+          .
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  whiteFont: PropTypes.bool
+};
+
+export default withStyles(footerStyle)(Footer);
---
+++ src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,57 @@
+/*eslint-disable*/
+import React from "react";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Link to={"/login-page"} className={classes.navLink}>
+          LOGIN
+        </Link>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Link to={"/register-page"} className={classes.navLink}>
+          REGISTER
+        </Link>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-twitter"
+          title="See source code of the project"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://github.com/anguyenguy"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+     
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
